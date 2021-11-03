f95038585253a4408c026db48884e0e44082f8a2
ManoDani <92886362+ManoDani@users.noreply.github.com>
2021-11-03 08:52:15 -0300

    1

--- ipbtoledo/application/phinx.php
+++ ipbtoledo/application/phinx.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
@@ -25,31 +24,3 @@ return [
         ],
     ],
 ];
-=======
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$dotenv = new \Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-
-return [
-    "paths" => [
-        "migrations" => __DIR__ . '/db/migrations',
-        "seeds" => __DIR__ . '/db/seeds'
-    ],
-    "environments" => [
-        "default_migration_table" => "migrations",
-        "default_database" => "development",
-        "development" => [
-            "adapter" => "mysql",
-            "name" => getenv('DB_NAME'),
-            "user" => getenv('DB_USER'),
-            "pass" => getenv('DB_PASS'),
-            "host" => getenv('DB_HOST'),
-            "charset" => getenv('DB_CHARSET'),
-            "collation" => "utf8_general_ci",
-        ],
-    ],
-];
->>>>>>> 9e6786124ee9095e51a202f022e0cd2f706854f3
